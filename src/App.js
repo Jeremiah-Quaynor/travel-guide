b03b9bc37d62df83f7b2599f32eff69531565a7f
Jeremiah-Quaynor <jeremiah.quaynor@stu.edu.gh>
2022-07-18 23:33:44 +0000

    building destination component

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Destinations from "./components/Destinations";
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Destinations />
+      <Search />
+    </div>
+  );
+}
+
+export default App;
